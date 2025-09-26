abd10034cd8b1fe2c3ca695dc022df7e7c975a9d
Antonio <98884381+AnthonyMoon69@users.noreply.github.com>
2025-09-26 19:54:11 +0200

    Enable credentialed CORS responses

--- config/cors.php
+++ config/cors.php
@@ -1,16 +1,30 @@
 <?php
 
+$origins = array_values(array_filter(array_map(
+    'trim',
+    explode(',', env('CORS_ALLOWED_ORIGINS', env('FRONTEND_URL', '')))
+)));
+
+$supportsCredentials = env('CORS_SUPPORTS_CREDENTIALS');
+$supportsCredentials = filter_var(
+    $supportsCredentials,
+    FILTER_VALIDATE_BOOLEAN,
+    ['flags' => FILTER_NULL_ON_FAILURE]
+);
+
+if (empty($origins)) {
+    $origins = ['*'];
+    $supportsCredentials = $supportsCredentials ?? false;
+} else {
+    $supportsCredentials = $supportsCredentials ?? true;
+}
+
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter(
-        array_map(
-            'trim',
-            explode(',', env('CORS_ALLOWED_ORIGINS', env('FRONTEND_URL', '')))
-        )
-    ) ?: ['*'],
+    'allowed_origins' => $origins,
 
     'allowed_origins_patterns' => [],
 
@@ -20,5 +34,5 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => $supportsCredentials,
 ];
